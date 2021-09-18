a0b6a38823cf047ce95fcd5d0728e73f3a3e48bb
Chrisi <jedchrisi09@gmail.com>
2021-09-18 13:31:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const discord = require("discord.js")
+const { token } = require("./config.json")
+const fs = require("fs")
+const Client = new discord.Client({
+    intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MEMBERS, discord.Intents.FLAGS.GUILD_MESSAGES, discord.Intents.FLAGS.DIRECT_MESSAGES],
+    allowedMentions: { parse: ["users", "roles"], repliedUser: true}
+})
+
+Client.on("ready", async () => {
+    Client.user.setPresence({activities: [{ name: `!help for ${Client.guilds.cache.size} Servers`, type: "WATCHING"}]})
+    console.log(`${Client.user.tag} is Online`)
+})
+
+Client.on("interactionCreate", async interaction => {
+
+})
+
+Client.on("messageCreate", async message => {
+    if(message.author.bot || message.channel.type == "DM") return
+})
+
+Client.login(token)
