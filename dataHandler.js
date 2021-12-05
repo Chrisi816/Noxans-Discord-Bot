c16090b1c1f4a77a313cb85dfe06c35ced129dc2
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2021-12-05 16:51:46 +0100

    Add files via upload

--- dataHandler.js
+++ dataHandler.js
@@ -45,4 +45,51 @@ async function createCmd(Client, guildId) {
     ]
     await Client.guilds.cache.get(guildId)?.commands.set(data)
 }
-module.exports = {createCmd}
+async function acCmd(Client, guildId) {
+    const data = [
+        {
+            name: "echo",
+            description: "Echo your text",
+            options: [{
+                name:"text",
+                type:"STRING",
+                description: "The input",
+                required: true 
+            }],
+        },
+        {
+            name: "ping",
+            description: "REPLY"
+        },
+        {
+            name: "jump",
+            description: "Jumping",
+            options: [{
+                name:"number",
+                type:"STRING",
+                description: "Die numer von dem Lied welches du spielen willst",
+                required: true 
+            }],
+        },
+        {
+            name: "play",
+            description: "play a song",
+            options: [{
+                name: "song",
+                description: "Spiele ein Lied ab",
+                type:"STRING",
+                required: true 
+            }],
+        },
+        {
+            name: "queue",
+            description: "Get the Queue",
+        },
+        {
+            name: "stop",
+            description: "Stop the Songs",
+        },
+    ]
+    await Client.guilds.cache.get(guildId)?.commands.set(data)
+}
+module.exports = {createCmd, acCmd}
--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,10 @@
 const Client =  require("../index").Client
 
-const { createCmd  } = require("../dataHandler")
+const { createCmd, acCmd } = require("../dataHandler")
 Client.on("ready", async () => {
     Client.user.setPresence({ activities: [{ name: `!help for ${Client.guilds.cache.size} Servers`, type: "WATCHING" }] })
     console.log(`${Client.user.tag} is Online`)
 
     createCmd(Client, "866729353252831242")
+    acCmd(Client, "775033960300019732")
   })
