90e8df76967cb6a6b4a79e003843cf7835a80d87
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2022-03-12 22:27:18 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,15 @@ const Client =  require("../index").Client
 
 const { createCmd, acCmd } = require("../dataHandler")
 Client.on("ready", async () => {
-    Client.user.setPresence({ activities: [{ name: `!help for ${Client.guilds.cache.size} Servers`, type: "WATCHING" }] })
+     let status = [
+      `!help for ${Client.guilds.cache.size} Servers`,
+      `for ${Client.users.cache.size} Members`
+    ]
+    Client.user.setStatus("dnd")
+    setInterval(() => {
+      let statuse = status[Math.floor(Math.random() * status.length)]
+      Client.user.setPresence({ activities: [{ name: statuse, type: "WATCHING" }]})
+    }, 5000)
     console.log(`${Client.user.tag} is Online`)
 
     createCmd(Client, "866729353252831242")
