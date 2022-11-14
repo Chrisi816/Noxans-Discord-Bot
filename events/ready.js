df10abc8e152767cdfb452c89ad4e504398dd6a2
Chrisi <81706150+Chrisi816@users.noreply.github.com>
2022-11-14 10:36:50 +0100

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,18 +1,28 @@
-const Client =  require("../index").Client
+const Client = require("../index").Client;
 
-const { createCmd, acCmd } = require("../dataHandler")
+const { createCmd, acCmd } = require("../dataHandler");
 Client.on("ready", async () => {
-     let status = [
-      `!help for ${Client.guilds.cache.size} Servers`,
-      `for ${Client.users.cache.size} Members`
-    ]
-    Client.user.setStatus("dnd")
-    setInterval(() => {
-      let statuse = status[Math.floor(Math.random() * status.length)]
-      Client.user.setPresence({ activities: [{ name: statuse, type: "WATCHING" }]})
-    }, 5000)
-    console.log(`${Client.user.tag} is Online`)
+	let status = [
+		`!help for ${Client.guilds.cache.size} Servers`,
+		`for ${Client.users.cache.size} Members`,
+	];
+	Client.user.setStatus("dnd");
+	setInterval(() => {
+		let statuse = status[Math.floor(Math.random() * status.length)];
+		Client.user.setPresence({
+			activities: [{ name: statuse, type: "WATCHING" }],
+		});
+	}, 5000);
+	console.log(`${Client.user.tag} is Online`);
+	let commands;
+	commands.create({
+		name: "ping",
+		description: "Replies with pong",
+	});
+	commands.create({
+		name: "truth",
+		description: "Replies with truth",
+	});
 
-    createCmd(Client, "866729353252831242")
-    acCmd(Client, "775033960300019732")
-  })
+	createCmd(Client, null);
+});
